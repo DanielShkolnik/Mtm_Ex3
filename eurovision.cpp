2dc8cbfc3e43d2066378ecd4cdc41b49924b2468
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-06-07 14:22:19 +0300

    New

---
+++ eurovision.cpp
@@ -0,0 +1,18 @@
+
+#include "eurovision.h"
+
+//*************************MAIN CONTROL**************************************
+
+MainControl::MainControl(int maxSongLength, int maxParticipants,
+                        int maxRegularTimesToVote): phase(Registration),
+                        participantScores(new ParticipantScore[maxParticipants]){
+
+
+
+}
+
+
+
+
+
+
