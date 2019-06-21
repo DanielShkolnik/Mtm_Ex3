f33d6cacff1f7379fe157b47b4572c45098a6482
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-06-21 17:42:57 +0300

    New

--- Part 1/seat.h
+++ Part 1/seat.h
@@ -5,6 +5,8 @@ using std::exception;
 using std::cout;
 using std::endl;
 
+const int NO_PRICE=0;
+
 // ---------------------------------------------
 class NoPrice: public exception
 {
@@ -22,20 +24,21 @@ public:
     virtual string location(){
         return  "line: " + std::to_string(this->line) + ", chair: " + std::to_string(this->chair);
     };
+    virtual ~Seat(){};
 
 protected:
     int line;
     int chair;
     int basePrice;
 public:
-    Seat(int line, int chair, int basePrice): line(line), chair(chair), basePrice(basePrice){}
+    Seat(int line, int chair, int basePrice=NO_PRICE): line(line), chair(chair), basePrice(basePrice){}
 };
 
 // ---------------------------------------------
 class GreenRoomSeat: public Seat
 {
 public:
-    GreenRoomSeat(int line, int chair): Seat(line,chair,10){}
+    GreenRoomSeat(int line, int chair): Seat(line,chair){}
     int price() override{
         throw NoPrice();
     }
@@ -46,43 +49,106 @@ public:
 };
 
 // ---------------------------------------------
-class MainHallSeat
+class MainHallSeat: public Seat
 {
+protected:
+    int const mainHallSeatPrice=100;
+public:
+    MainHallSeat(int line, int chair, int basePrice):Seat(line,chair,basePrice){}
 };
 
 // ---------------------------------------------
-class SpecialSeat
+class SpecialSeat:public MainHallSeat
 {
+protected:
+    int const specialSeatPrice=300;
+public:
+    SpecialSeat(int line, int chair, int basePrice):MainHallSeat(line,chair,basePrice){}
 };
 
 // ---------------------------------------------
-class GoldenCircleSeat
+class GoldenCircleSeat:public SpecialSeat
 {
+private:
+    int const goldenCircleSeatPrice=1000;
+public:
+    GoldenCircleSeat(int line, int chair, int basePrice):SpecialSeat(line,chair,basePrice){}
+    string location() override{
+        return  "Golden Circle-> " + Seat::location();
+    }
+    int price() override{
+        return (Seat::basePrice+MainHallSeat::mainHallSeatPrice+SpecialSeat::specialSeatPrice+goldenCircleSeatPrice);
+    }
 };
 
 // ---------------------------------------------
-class DisablePodiumSeat
+class DisablePodiumSeat:public SpecialSeat
 {
+private:
+    int const disablePodiumSeatPrice=200;
+public:
+    DisablePodiumSeat(int line, int chair, int basePrice=NO_PRICE):SpecialSeat(line,chair,basePrice){}
+    string location() override{
+        return  "Disable Podium-> " + Seat::location();
+    }
+    int price() override{
+        return disablePodiumSeatPrice;
+    }
 };
 
 // ---------------------------------------------
-class RegularSeat
+class RegularSeat:public MainHallSeat
 {
+private:
+    char area;
+public:
+    RegularSeat(char area, int line, int chair, int basePrice):MainHallSeat(line,chair,basePrice), area(area){}
+    string location() override{
+        return  "area: " + std::basic_string.append(this->area) + Seat::location();
+    };
 };
 
 // ---------------------------------------------
-class FrontRegularSeat
+class FrontRegularSeat:public RegularSeat
 {
+private:
+    int const frontRegularSeatPrice=500;
+public:
+    FrontRegularSeat(char area, int line, int chair, int basePrice):RegularSeat(area,line,chair,basePrice){}
+    string location() override{
+        return  "Front-> " + RegularSeat::location();
+    }
+    int price() override{
+        return (Seat::basePrice+MainHallSeat::mainHallSeatPrice+frontRegularSeatPrice);
+    }
 };
 
 // ---------------------------------------------
-class MiddleRegularSeat
+class MiddleRegularSeat:public RegularSeat
 {
+private:
+    int const middleRegularSeatPrice=250;
+public:
+    MiddleRegularSeat(char area, int line, int chair, int basePrice):RegularSeat(area,line,chair,basePrice){}
+    string location() override{
+        return  "Middle-> " + RegularSeat::location();
+    }
+    int price() override{
+        return (Seat::basePrice+MainHallSeat::mainHallSeatPrice+middleRegularSeatPrice);
+    }
 };
 
 // ---------------------------------------------
-class RearRegularSeat
+class RearRegularSeat:public RegularSeat
 {
+public:
+    RearRegularSeat(char area, int line, int chair, int basePrice):RegularSeat(area,line,chair,basePrice){}
+    string location() override{
+        return  "Middle-> " + RegularSeat::location();
+    }
+    int price() override{
+        return (Seat::basePrice+MainHallSeat::mainHallSeatPrice);
+    }
 };
 
 // ---------------------------------------------
--- Part 1/test_part_c.cpp
+++ Part 1/test_part_c.cpp
@@ -41,78 +41,78 @@ TEST("C.3")
        cout << e.what() << endl;
    }
 
-TEST("C.4")
+    TEST("C.4")
 //   MainHallSeat mhs1(30, 50, 100); /* line 30, chair 50, base price 100 */
 //                                  /* compilation error. can not define an object of this type. */
 
-TEST("C.5")
+    TEST("C.5")
 //   RegularSeat rs1('B', 25, 15, 100); /* area B, line 25, chair 15, base price 100 */
 //                                      /* compilation error.  can not define an object of this type. */
 
 
-TEST("C.6")
-FrontRegularSeat frs1('C', 8, 20, 100);  /* area C, line 8, chair 20, base price 100 */
-cout << frs1.location() << endl;
-cout << frs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
+    TEST("C.6")
+    FrontRegularSeat frs1('C', 8, 20, 100);  /* area C, line 8, chair 20, base price 100 */
+    cout << frs1.location() << endl;
+    cout << frs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
                                     another addition of 500 for FrontRegularSeat. */
 
-TEST("C.7")
-MiddleRegularSeat mrs1('D', 30, 15, 80); /* area D, line 30, chair 15, base price 80 */
-cout << mrs1.location() << endl;
-cout << mrs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
+    TEST("C.7")
+    MiddleRegularSeat mrs1('D', 30, 15, 80); /* area D, line 30, chair 15, base price 80 */
+    cout << mrs1.location() << endl;
+    cout << mrs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
                                     another addition of 250 for MiddleRegularSeat. */
 
-TEST("C.8")
-RearRegularSeat rrs1('E', 60, 18, 70);    /* area E, line 60, chair 18, base price 70 */
-cout << rrs1.location() << endl;
-cout << rrs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat.
+    TEST("C.8")
+    RearRegularSeat rrs1('E', 60, 18, 70);    /* area E, line 60, chair 18, base price 70 */
+    cout << rrs1.location() << endl;
+    cout << rrs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat.
                                     No other addition */
 
-TEST("C.9")
+    TEST("C.9")
 //   SpecialSeat sps1(1, 17, 100); /* line 1, chair 17, base price 100 */
 //                                  /* compilation error. can not define an object of this type. */
 
-TEST("C.10")
-GoldenCircleSeat gcs1(2, 14, 100);  /* no area for any SpecialSeat, specifically nor for GoldenCircleSeat which counts
+    TEST("C.10")
+    GoldenCircleSeat gcs1(2, 14, 100);  /* no area for any SpecialSeat, specifically nor for GoldenCircleSeat which counts
                                           as SpecialSeat.
                                           we get here line 2, chair 14, base price 100 */
-cout << gcs1.location() << endl;
-cout << gcs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
+    cout << gcs1.location() << endl;
+    cout << gcs1.price() << endl; /* price is base price + addition of 100 for MainHallSeat +
                                     another addition of 300 for SpecialSeat + another addition of 100 for GoldenCircleSeat.
                                     Total of 100+100+300+1000 = 1500 */
 
-TEST("C.11")
-DisablePodiumSeat dps1(1, 10, 100);  /* no area for any SpecialSeat, specifically nor for DisablePodiumSeat which counts
+    TEST("C.11")
+    DisablePodiumSeat dps1(1, 10, 100);  /* no area for any SpecialSeat, specifically nor for DisablePodiumSeat which counts
                                           as SpecialSeat.
                                           we get here line 1, chair 10, base price 100 (base price is meaningless here) */
-cout << dps1.location() << endl;
-cout << dps1.price() << endl; /* price is fixed 200, with no other additions, nor base price. */
+    cout << dps1.location() << endl;
+    cout << dps1.price() << endl; /* price is fixed 200, with no other additions, nor base price. */
 
-TEST("C.12")
-DisablePodiumSeat dps2(1, 11);
-cout << dps2.location() << endl;
-cout << dps2.price() << endl;
+    TEST("C.12")
+    DisablePodiumSeat dps2(1, 11);
+    cout << dps2.location() << endl;
+    cout << dps2.price() << endl;
 
-TEST("C.13")
-const int len=6;
-Seat* seats[len] = {new GreenRoomSeat(3,8), new FrontRegularSeat('A',5,8,150), new MiddleRegularSeat('B',25,35,120),
-                    new RearRegularSeat('E',70,4,90), new GoldenCircleSeat(2,19,200),
-                    new DisablePodiumSeat(1,5)};
+    TEST("C.13")
+    const int len=6;
+    Seat* seats[len] = {new GreenRoomSeat(3,8), new FrontRegularSeat('A',5,8,150), new MiddleRegularSeat('B',25,35,120),
+                        new RearRegularSeat('E',70,4,90), new GoldenCircleSeat(2,19,200),
+                        new DisablePodiumSeat(1,5)};
 
-for (int i=0; i<len; ++i)
-{
-cout << seats[i]->location() << endl;
-try
-{
-cout << seats[i]->price() << endl;
-}
-catch (exception& e)
-{
-cout << e.what() << endl;
-}
-}
+    for (int i=0; i<len; ++i)
+    {
+        cout << seats[i]->location() << endl;
+        try
+        {
+            cout << seats[i]->price() << endl;
+        }
+        catch (exception& e)
+        {
+            cout << e.what() << endl;
+        }
+    }
 
-for (Seat** p=seats; p<seats+len; ++p) delete *p;
+    for (Seat** p=seats; p<seats+len; ++p) delete *p;
 
 return 0;
 }
