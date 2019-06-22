cde724c460f1caebed10a49402a10fb6ff29421d
OmerHemo <OmerHemo@users.noreply.github.com>
2019-06-22 12:06:04 +0300

    dry

--- dry/main.cpp
+++ dry/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class Base
+{
+    void virtual method() {std::cout << "from Base" << std::endl;}
+public:
+    virtual ~Base() {method();}
+    Base(){method();}
+    void baseMethod() { method();}
+};
+class A: public Base
+{
+    void method() {std::cout << "from A" << std::endl;}
+public:
+    ~A () { method();}
+    A():Base(){method();}
+};
+
+int main (void)
+{
+    Base* base = new A();
+    base -> baseMethod();
+    delete base;
     return 0;
 }
 
